1888d76b018a3a0e6f3daf463e338742039b7fa3
Sean Hellum <seanhellum45@gmail.com>
2020-01-07 18:13:21 +0000

    initialize project

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('main.html', message=message)
+
+# run the application
+if __name__ == "__main__":
+    app.run(port=3000)
+
